846d159a47b362139e83cf961e5fa59deddadbd3
Lesliem Lopez <leslieml@proton.me>
2025-01-08 12:53:10 -0600

    add exercises of section or unit four

---
+++ module-one/src/main/java/workbooks/unitFour/Workbook41.java
@@ -0,0 +1,24 @@
+package workbooks.unitFour;
+
+public class Workbook41 {
+    public static void main(String[] args) {
+        printLines();
+        printLines();
+        printLines();
+        printLines();
+        printLines();
+        printLines();
+    }
+    /**
+     * Function name: printLines
+     *
+     * Inside the function:
+     *   1. prints the four lines
+     */
+    public static void printLines(){
+        System.out.println("I will not copy and paste code.");
+        System.out.println("I will use Camel Case when writing class names.");
+        System.out.println("I will use lower Camel Case when writing function name.");
+        System.out.println("I will use lower Camel Case when writing variable names.");
+    }
+}
